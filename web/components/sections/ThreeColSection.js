cdccaf3eca8a2bdb0d0f33d68c33f20dfd88755c
Daniel Kapper <dkapper01@gmail.com>
2020-01-16 10:17:28 -0500

    added three col section to web

---
+++ web/components/sections/ThreeColSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ThreeColSection = (props) => {
+  console.log(props)
+  return (
+    <div>
+      TEST
+    </div>
+  )
+}
+
+export default ThreeColSection
