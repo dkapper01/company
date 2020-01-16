e90a1b58c1d60f997adb0604aefe95087e179d59
Daniel Kapper <dkapper01@gmail.com>
2020-01-16 10:30:17 -0500

    set up three col section working

--- web/components/sections/ThreeColSection.js
+++ web/components/sections/ThreeColSection.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import SimpleBlockContent from '../SimpleBlockContent'
 
-const ThreeColSection = (props) => {
+const ThreeColSection = props => {
+  const {
+    heading,
+    titleOne,
+    titleTwo,
+    titleThree,
+    shortDescriptionOne,
+    shortDescriptionTwo,
+    shortDescriptionThree
+  } = props
   console.log(props)
-  return (
+  return <div>
+    <h2>{heading}</h2>
     <div>
-      TEST
+      {titleOne}
+      <SimpleBlockContent blocks={shortDescriptionOne} />
     </div>
-  )
+    <div>
+      {titleTwo}
+      <SimpleBlockContent blocks={shortDescriptionTwo} />
+    </div>
+    <div>
+      {titleThree}
+      <SimpleBlockContent blocks={shortDescriptionThree} />
+
+    </div>
+  </div>
+}
+
+ThreeColSection.propTypes = {
+  heading: PropTypes.string,
+  titleOne: PropTypes.string,
+  titleTwo: PropTypes.string,
+  titleThree: PropTypes.string,
+  shortDescriptionOne: PropTypes.array,
+  shortDescriptionTwo: PropTypes.array,
+  shortDescriptionThree: PropTypes.array
 }
 
 export default ThreeColSection
